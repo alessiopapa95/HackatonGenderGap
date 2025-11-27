1c3dce7eff5f0bcfd44a99382a4bd2138f41ccf9
alessiopapa95 <136860541+alessiopapa95@users.noreply.github.com>
2025-11-27 10:18:37 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import javafx.application.Application;
 import dashboard.Dashboard;
 
-//HOLA!!
-//HELLO!!
-
 public class Main {
     public static void main(String[] args) {
         Application.launch(Dashboard.class, args);
